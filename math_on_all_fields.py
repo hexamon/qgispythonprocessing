8eda32b1c57dfd3eec9e957a58bd15ea9342dcf6
Anton W <anton.a.westholm@gmail.com>
2019-10-28 15:58:13 +0100

    Add files via upload

---
+++ math_on_all_fields.py
@@ -0,0 +1,227 @@
+# -*- coding: utf-8 -*-
+
+"""
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************
+"""
+
+from qgis.PyQt.QtCore import QCoreApplication
+from qgis.core import (QgsProcessing,
+                       QgsFeatureSink,
+                       QgsProcessingException,
+                       QgsProcessingAlgorithm,
+                       QgsProcessingParameterFeatureSource,
+                       QgsProcessingParameterField,
+                       QgsProcessingParameterEnum,
+                       QgsProcessingParameterFeatureSink)
+from qgis import processing
+
+
+class MathOnAllFields(QgsProcessingAlgorithm):
+    """
+    Run a math operation on all fields of a vector layer.
+    """
+
+    # Constants used to refer to parameters and outputs. They will be
+    # used when calling the algorithm from another algorithm, or when
+    # calling from the QGIS console.
+
+    INPUT = 'INPUT'
+    OUTPUT = 'OUTPUT'
+    DIVCOL = 'DIVCOL'
+    OPERATION = 'OPERATION'
+
+    def tr(self, string):
+        """
+        Returns a translatable string with the self.tr() function.
+        """
+        return QCoreApplication.translate('Processing', string)
+
+    def createInstance(self):
+        return MathOnAllFields()
+
+    def name(self):
+        """
+        Returns the algorithm name, used for identifying the algorithm. This
+        string should be fixed for the algorithm, and must not be localised.
+        The name should be unique within each provider. Names should contain
+        lowercase alphanumeric characters only and no spaces or other
+        formatting characters.
+        """
+        return 'math_all_cols'
+
+    def displayName(self):
+        """
+        Returns the translated algorithm name, which should be used for any
+        user-visible display of the algorithm name.
+        """
+        return self.tr('Math on all attributes')
+
+    def group(self):
+        """
+        Returns the name of the group this algorithm belongs to. This string
+        should be localised.
+        """
+        return self.tr('Attribute processing')
+
+    def groupId(self):
+        """
+        Returns the unique ID of the group this algorithm belongs to. This
+        string should be fixed for the algorithm, and must not be localised.
+        The group id should be unique within each provider. Group id should
+        contain lowercase alphanumeric characters only and no spaces or other
+        formatting characters.
+        """
+        return 'attr_processing'
+
+    def shortHelpString(self):
+        """
+        Returns a localised short helper string for the algorithm. This string
+        should provide a basic description about what the algorithm does and the
+        parameters and outputs associated with it..
+        """
+        return self.tr("Runs a math operation on all numeric attributes by using another attribute")
+
+    def initAlgorithm(self, config=None):
+        """
+        Here we define the inputs and output of the algorithm, along
+        with some other properties.
+        """
+
+        # We add the input vector features source. It can have any kind of
+        # geometry.
+        self.addParameter(
+            QgsProcessingParameterFeatureSource(
+                self.INPUT,
+                self.tr('Input layer'),
+                [QgsProcessing.TypeVectorAnyGeometry]
+            )
+        )
+
+        # We add a feature sink in which to store our processed features (this
+        # usually takes the form of a newly created vector layer when the
+        # algorithm is run in QGIS).
+        self.addParameter(
+            QgsProcessingParameterFeatureSink(
+                self.OUTPUT,
+                self.tr('Output layer')
+            )
+        )
+        
+        #Column to use for mathematical operation.
+        self.addParameter(
+            QgsProcessingParameterField(
+                self.DIVCOL,
+                self.tr('Column to use for operation. All columns will be multiplied/divided/etc by this value.'),
+                'h',
+                self.INPUT
+            )
+        )
+
+        #Mathematical operation to perform.
+        self.addParameter(
+            QgsProcessingParameterEnum(
+                self.OPERATION,
+                self.tr('Mathematical operation to perform.'),
+                options=[self.tr('Multiply'),
+                self.tr('Divide'),
+                self.tr('Add'),
+                self.tr('Substract')],
+                defaultValue=0,
+                optional=False
+            )
+        )     
+
+    def processAlgorithm(self, parameters, context, feedback):
+        """
+        Here is where the processing itself takes place.
+        """
+
+        # Retrieve the feature source and sink. The 'dest_id' variable is used
+        # to uniquely identify the feature sink, and must be included in the
+        # dictionary returned by the processAlgorithm function.
+        source = self.parameterAsSource(
+            parameters,
+            self.INPUT,
+            context
+        )
+
+        # If source was not found, throw an exception to indicate that the algorithm
+        # encountered a fatal error. The exception text can be any string, but in this
+        # case we use the pre-built invalidSourceError method to return a standard
+        # helper text for when a source cannot be evaluated
+        if source is None:
+            raise QgsProcessingException(self.invalidSourceError(parameters, self.INPUT))
+
+        (sink, dest_id) = self.parameterAsSink(
+            parameters,
+            self.OUTPUT,
+            context,
+            source.fields(),
+            source.wkbType(),
+            source.sourceCrs()
+        )
+
+        # Send some information to the user
+        feedback.pushInfo('CRS is {}'.format(source.sourceCrs().authid()))
+
+        # If sink was not created, throw an exception to indicate that the algorithm
+        # encountered a fatal error. The exception text can be any string, but in this
+        # case we use the pre-built invalidSinkError method to return a standard
+        # helper text for when a sink cannot be evaluated
+        if sink is None:
+            raise QgsProcessingException(self.invalidSinkError(parameters, self.OUTPUT))
+
+        # Compute the number of steps to display within the progress bar and
+        # get features from source
+        total = 100.0 / source.featureCount() if source.featureCount() else 0
+        features = source.getFeatures()
+        
+        op = self.parameterAsEnum(parameters, self.OPERATION,context) #Operation fetched from parameter
+        col = self.parameterAsFields(parameters, self.DIVCOL,context)[0] #Colum for operation
+
+        for current, feature in enumerate(features):
+            
+            # Stop the algorithm if cancel button has been clicked
+            if feedback.isCanceled():
+                break
+            
+            for current, attr in enumerate(feature.fields()):
+                
+                if current != feature.fieldNameIndex(col):
+                
+                    c = feature.attribute(feature.fieldNameIndex(attr.name()))
+                    d = feature.attribute(feature.fieldNameIndex(col))
+                    
+                    if(attr.isNumeric() == TRUE):
+                    
+                        if(op == 0):
+                            newVal = c * d
+                        elif(op == 1):
+                            newVal = c / d
+                        elif(op == 2):
+                            newVal = c + d
+                        elif(op == 3):
+                            newVal = c - d
+                        
+                        feature.setAttribute(current, newVal)
+            
+            # Add a feature in the sink
+            sink.addFeature(feature, QgsFeatureSink.FastInsert)
+
+            # Update the progress bar
+            feedback.setProgress(int(current * total))
+
+        # Return the results of the algorithm. In this case our only result is
+        # the feature sink which contains the processed features, but some
+        # algorithms may return multiple feature sinks, calculated numeric
+        # statistics, etc. These should all be included in the returned
+        # dictionary, with keys matching the feature corresponding parameter
+        # or output names.
+        return {self.OUTPUT: dest_id}
